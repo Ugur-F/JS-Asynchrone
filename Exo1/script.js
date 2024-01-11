d98fed9308b7e6798e2c5e74ca69ea37f63190e0
Ugur-F <ugur.firat240@gmail.com>
2024-01-11 13:57:48 +0100

    Exo1

---
+++ Exo1/script.js
@@ -0,0 +1,15 @@
+//Script
+document.getElementById('Btn').addEventListener('click', function() {
+    fetch('file.json') 
+        .then(response => response.json())
+        .then(data => {
+            const list = document.getElementById('crewList');
+            list.innerHTML = '';
+            data.members.forEach(member => {
+                const listItem = document.createElement('li');
+                listItem.textContent = `${member.role} : ${member.name} - ${member.powers}`;
+                list.appendChild(listItem);
+            });
+        })
+        .catch(error => console.error('Erreur lors du chargement du fichier JSON:', error));
+});
